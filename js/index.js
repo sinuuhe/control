75bb707004703ae930dad6912ac6d8a980797baa
Sinuhe Gomez <sinuhegomezvalencia@gmail.com>
2018-04-11 01:33:27 -0500

    pdf donde

--- js/index.js
+++ js/index.js
@@ -379,12 +379,12 @@ function registerEmploye(){
 function query(findablePath,fieldsArray,tableId,filters){
     var printBtn = document.getElementById('printReport');
     printBtn.classList.remove('hide');
-    //printBtn.setAttribute("onclick","makePDF();");
+
 
     var result = database.ref(findablePath + '/').orderByChild('name');
     var resultArray;
     var table = document.getElementById(tableId);
-    table.innerHTML = "<colgroup><col width='20%'><col width='20%'><col width='20%'><col width='20%'><colgroup>";
+    
     var tableHead, tableBody;
     tableHead = "<thead><tr>"
     tableBody = "<tbody>"
@@ -531,7 +531,6 @@ function query(findablePath,fieldsArray,tableId,filters){
             cleanActiveInputs("" + filters);
         }           
     });
-    console.log(table.innerHTML);
 };
 
 
@@ -802,90 +801,33 @@ document.getElementById('activeNameInput').value = "";
 
 };
 
-function makePDF(){     
+function makePDF(){
+    var date = new Date();
     
-    var pdf = new jsPDF('p', 'pt', 'letter');
-    // source can be HTML-formatted string, or a reference
-    // to an actual DOM element from which the text will be scraped.
-    source = $('#resultsTable')[0];
-
-    // we support special element handlers. Register them with jQuery-style 
-    // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
-    // There is no support for any other type of selectors 
-    // (class, of compound) at this time.
-    specialElementHandlers = {
-        // element with id of "bypass" - jQuery style selector
-        '#bypassme': function (element, renderer) {
-            // true = "handled elsewhere, bypass text extraction"
-            return true
-        }
-    };
-    margins = {
-        top: 80,
-        bottom: 60,
-        left: 40,
-        width: 522
-    };
-    // all coords and widths are in jsPDF instance's declared units
-    // 'inches' in this case
-    pdf.fromHTML(
-    source, // HTML string or DOM elem ref.
-    margins.left, // x coord
-    margins.top, { // y coord
-        'width': margins.width, // max width of content on PDF
-        'elementHandlers': specialElementHandlers
-    },
-
-    function (dispose) {
-        // dispose: object with X, Y of the last line add to the PDF 
-        //          this allow the insertion of new lines after html
-        pdf.save('Test.pdf');
-    }, margins);
-};
-
-function tableToJson(table) {
-    var data = [];
-
-    // first row needs to be headers
-    var headers = [];
-    for (var i=0; i<table.rows[0].cells.length; i++) {
-        headers[i] = table.rows[0].cells[i].innerHTML.toLowerCase().replace(/ /gi,'');
-    }
-    data.push(headers);
-    // go through cells
-    for (var i=1; i<table.rows.length; i++) {
-
-        var tableRow = table.rows[i];
-        var rowData = {};
-
-        for (var j=0; j<tableRow.cells.length; j++) {
-
-            rowData[ headers[j] ] = tableRow.cells[j].innerHTML;
-
-        }
-
-        data.push(rowData);
-    }       
-
-    return data;
-};
-
-function callme(){
-    var table = tableToJson($('#resultsTable').get(0));
-    var doc = new jsPDF('l','pt','letter',true);
-    
-    
-    $.each(table, function(i, row){
-        $.each(row, function(j,cell){
-        if(j=="modelo" | j==0){
-         doc.cell(1,10,190,20,cell,i);	
-        }
-        else{
-            doc.cell(1,10,90,20,cell,i);
-        }
-        
-        });
-    });
-    
-    doc.save('Safaa.pdf');
-    };
+    jsPDF.autoTableSetDefaults({headerStyles: {fillColor: [62, 39, 35]}});
+   var doc = new jsPDF('l', 'pt');
+ 
+ var res = doc.autoTableHtmlToJson(document.getElementById("resultsTable"));
+ var cols = [res.columns[0],res.columns[1],res.columns[2],res.columns[3],res.columns[4],res.columns[5]];
+ //doc.autoTable(cols, res.data, {margin: {top: 80}});
+ var header = function(data) {
+   doc.setFontSize(18);
+   doc.setTextColor(40);
+   doc.setFontStyle('normal');
+   //doc.addImage(headerImgData, 'JPEG', data.settings.margin.left, 20, 50, 50);
+   doc.text("Reporte " + date.toLocaleDateString(), data.settings.margin.left, 20);
+ };
+ 
+ var options = {
+     theme: 'grid',
+   addPageContent: header,
+   margin: {
+     top: 100
+   },
+   startY: doc.autoTableEndPosY() + 40
+ };
+ 
+ doc.autoTable(cols, res.data, options);
+ 
+ doc.save("reporte" + date.toLocaleDateString() + ".pdf");
+ };
